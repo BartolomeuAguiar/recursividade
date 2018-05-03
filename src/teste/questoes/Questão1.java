676353a0e6d571c6f9df8d35c9aebb8f67fba4e1
antonio.abnma <antonio.abnma@PMASTER_BARTO.pf.gov.br>
2018-05-03 18:52:15 -0400

    questoes de 1 a 7

--- src/teste/questoes/Questão1.java
+++ src/teste/questoes/Questão1.java
@@ -4,7 +4,6 @@ import funcao.recursivas.Fibonacci;
 public class Questão1 {
 	public static void main(String[] args) {
 		int posicao = 12;
-		//System.out.println(Fibonacci.fibonacci(posicao));
 		for (int i = 1; i <= posicao; i++) {
 			System.out.println(Fibonacci.fibonacci(i));
 		}
